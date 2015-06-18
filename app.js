60cc0f82925596e8a4dc54fe896e5407fa806a09
Paul Cullen Rowe <prowe214@gmail.com>
2015-06-18 15:52:05 -0600

    created tic tac toe file

---
+++ app.js
@@ -0,0 +1,76 @@
+var turn = 1,
+    x = [],
+    o =[],
+    winners = [
+              [1, 2, 3],
+              [4, 5, 6],
+              [7, 8, 9],
+              [1, 4, 7],
+              [2, 5, 8],
+              [3, 6, 9],
+              [1, 5, 9],
+              [3, 5, 7]
+              ],
+    box = document.getElementsByClassName('box')
+
+for (var i = 0; i < box.length; i++) {
+  box[i].points = i + 1
+  box[i].addEventListener('click', function() {
+      console.log('turn = ' + turn)
+    if (this.innerHTML === '') {
+      if (turn % 2 === 0) {
+        o.push(this.points)
+        o.sort()
+        this.innerHTML = '<p>O</p>'
+        checkWinner(o)
+      } else {
+        x.push(this.points)
+        x.sort()
+        this.innerHTML = '<p>X</p>'
+        checkWinner(x)
+      }
+      turn++
+    }
+  })
+}
+
+function checkWinner (arr) {
+  for (var i = 0; i < winners.length; i++) {
+  var xp = 0,
+      op = 0
+    for (var j = 0; j < arr.length; j++) {
+      if (winners[i].indexOf(arr[j]) > -1) {
+        if (turn % 2 === 0) {
+          op++
+          console.log('op = ' + op)
+        } else {
+          xp++
+          console.log('xp = ' + xp)
+        }
+      }
+    }
+  }
+    if (xp === 3) {
+        if (turn % 2 === 0) {
+          alert('O wins!')
+        } else {
+          alert('X wins!')
+        }
+      boardreset()
+    }
+    if (turn === 9) {
+      alert('Draw!')
+      boardreset()
+    }
+    console.log('turn = ' + turn)
+}
+
+function boardreset () {
+  turn = 0
+  x = []
+  o = []
+  for (var i = 0; i < box.length; i++) {
+    box[i].innerHTML = ''
+    box[i].points = 0
+  }
+}
